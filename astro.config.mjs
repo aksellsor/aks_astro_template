faea621a4f3c68335e0d818cd2162290c32fbdf8
lassespilling <lassespilling@gmail.com>
2024-08-09 16:43:08 +0200

    fix astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
-import { loadEnv } from 'vite';
 import { qrcode } from 'vite-plugin-qrcode';
 
 // Fix SSL umbraco api locally
 if (process.env.NODE_ENV === 'development') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 }
-
-const { PUBLIC_LOCALES, PUBLIC_DEFAULT_LOCALE } = loadEnv(
-  import.meta.env,
-  process.cwd(),
-  '',
-);
 
 export default defineConfig({
   vite: {
@@ -23,14 +16,7 @@ export default defineConfig({
       }),
     ],
   },
-  output: 'server',
-  adapter: cloudflare({ imageService: 'passthrough' }),
   integrations: [react()],
-  i18n: {
-    defaultLocale: PUBLIC_DEFAULT_LOCALE,
-    locales: PUBLIC_LOCALES.split(','),
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
+  output: 'server',
+  adapter: cloudflare({ imageService: 'noop' }),
 });
