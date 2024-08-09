ec2f5b1296b355de124d613f4d2a059abcbe02b0
lassespilling <lassespilling@gmail.com>
2024-08-09 09:45:11 +0200

    add deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 
 // Fix for querying local Umbraco project
 import cloudflare from '@astrojs/cloudflare';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+
+// Fix SSL umbraco api locally
 if (process.env.NODE_ENV === 'development') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 }
-
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  integrations: [react()]
+  integrations: [react()],
 });
